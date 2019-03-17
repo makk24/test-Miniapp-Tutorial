332c642bfaf22b9572fce03cbbd7bd097f3d8190
kk <1009576562@qq.com>
2019-03-17 22:17:04 +0800

    1

---
+++ utils/config.js
@@ -0,0 +1,13 @@
+const APPID = "1f76855d2b974a03a6985352df178c4d";
+
+if(APPID === ""){
+  wx.showToast({
+    title: `请在config.js中提供正确的appid`,
+    icon: 'none',
+    duration: 5000
+  });
+}
+
+module.exports = {
+  APPID: APPID
+}
