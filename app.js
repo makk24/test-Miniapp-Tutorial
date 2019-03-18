39c028ca88554df18068f1a62b77d1a0aa53b8b4
kk <1009576562@qq.com>
2019-03-18 23:50:29 +0800

    init

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-const Utils = require("./utils/util.js");
-
-//app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    Utils.checkSystemInfo(this);
+  onLaunch: function (options) {
 
-    wx.authorize({
-      scope: 'scope.record',
-    });
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
   }
 })
---
+++ pages/webrtc-room/account.js
@@ -0,0 +1,23 @@
+//将 TEST_ACCOUNT 的内容替换成"控制台/实时音视频/快速上手第三步"生成的配置文件内容
+const TEST_ACCOUNT = { 
+  "sdkappid": 1400192988, 
+  "users": [{ 
+    "userId": "Miniprogram_trtc_01", 
+    "userToken": "eJxlj01Pg0AURff8CsJWY2eAaRgTF9r0g7ZaK1bBzQSZB461MBleK8T4341UI4l3e07uzf2wbNt27pfRWZpl1b5Ega0Gxz63HeKc-kGtlRQpCs-IfxAarQyINEcwHaSMMZeQvqMklKhy9WNcq1JpUxUm3Qk0mAlCe3Itt6JbPLb5hFDu8iDoK6o4Fo03o3A92jQNRGyOwyWwcPq8eCM1YBLpLfNxEcUJv11JL4wfpX8ZFvEqX*vXwY13kk9n70-JhOkQJvuHQZJcUX4oWdbOX9p4NrwbX-QmUe3g957LKQl4-*ABTK2qshNcQhl1PfIdx-q0vgCFNGHq" 
+    }, 
+    {
+       "userId": "Miniprogram_trtc_02", 
+       "userToken": "eJxlj1FPgzAUhd-5FYRno7dFHDXxgW2gLCMbupnoS1NpR64IdKVbJMb-bmQaSTyv35dzcj4c13W9zfLhXBRFe2gst71WnnvteuCd-UGtUXJhuW-kP6jeNRrFxc4qM0ASBAEFGDsoVWNxhz9Ghg1q05ZG1NwaW3CgI7mTFR8WT22XAIRRFoZjBctTUbydpdN*f0hYPJ9fHLNKpUm*nKyy1f2aHWaZUL0yeioZJUxFMsI4YvVddyUWz4-7ZPLyKqvNrc6zNrF5WeawCKHaEnzyw7cyrW5GkxZr9XuPMgIhGx88KtNh2wwCBRIQ6sN3POfT*QJ5oWHp" 
+    },
+    { 
+      "userId": "Miniprogram_trtc_03", 
+      "userToken":        "eJxlj0FPg0AYRO-8CsJVY3YXtoJJDxRqQwO1KjbRywZhC18IsF22lGL870aqkcS5vpeZzIem67oRh883SZo2x1oxdRbc0O90AxnXf1AIyFiimCmzf5D3AiRnyV5xOUJMKSUITR3IeK1gDz9GBDUI2eQyqZiSKmXInMhtVrJx8dJmIYQd4tj2VIH8UrR89AJ-7RN6FdYPr*dycxoOu5k9wOItiPx4s1i27zj2rJc85V1hueBmu44eReK7YZ-35uqUNFVUFodZUERPzf12xQflkTVOb4N8Pp9MKqj47z3iYGQ704Mdly009SgQhCkmJvqOoX1qX8fGYqo_"
+      },
+      { 
+        "userId": "Miniprogram_trtc_04",
+         "userToken": "eJxlj8FOhDAARO98BeGqkbYUt5h4YA0Sg7qrrlk5NbUUbJTSbYuCxn83shpJnOt7mcl8eL7vB5vLuyPGedcrR92oReCf*AEIDv*g1rKizNHIVP*gGLQ0grLaCTNBGMcxAmDuyEooJ2v5Y1xJJbXpGsNa6ozjFOCZbKtnOi3u2zAAMEEJIXNFNvui7P7sIid2DN8G0RNT8pf3og6zarmDy*2NLvTI8wPGH*tbsF7bKJVZCnGKecnydljY3Qpvn9RKXlt83DUPi7DoUduVBJ4PsdrY09mkk634vYcSCEgyP-gqjJWdmgQEYAxRBL4TeJ-eF*p6Yq8_"
+      }
+     ]
+  }
+
+module.exports = TEST_ACCOUNT;
